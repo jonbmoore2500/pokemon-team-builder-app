81a7faeafce5e1d4b811e3cdf46192a37c6cd3d0
Jonathan <jonathanmoorevla@gmail.com>
2022-10-08 21:23:27 -0500

    Add filter by type to Explore

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,18 +1,24 @@
-import React, {useContext} from "react"
+import React, {useContext, useState} from "react"
 import PokemonDisp from "./PokemonDisp.js"
 import { Container } from "semantic-ui-react"
 import {PokemonContext} from "../contexts/PokemonContext.js"
+import Filter from "./Filter.js"
 
 function Explore() {
     const {pokemonArr} = useContext(PokemonContext)
+    const [filterVal, setFilterVal] = useState('all')
+    const displayArr = pokemonArr.filter((pokemon) => filterVal === 'all' || pokemon.types.find(type => type === filterVal))
+    
+    function onFilterChange(typeToFilter) {
+        setFilterVal(typeToFilter)
+    }
+
 
     return (
         <div className="ui container">
             <h1>Learn about the available pokemon here</h1>
-            {/* add filter? 
-            alphabetical? reverse alphabetical? type? hp? favorite (persistent or not)? can evolve?
-            */}
-            <PokemonDisp pokemonArr={pokemonArr}/>
+            <Filter onFilterChange={onFilterChange}/>
+            <PokemonDisp pokemonArr={displayArr}/>
         </div>
     )
 }
---
+++ src/components/Filter.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+function Filter({onFilterChange}) {
+
+
+    function handleFilter(e) {
+        onFilterChange(e.target.value)
+    }
+
+    return (
+        <div className="ui search">
+            <div className="ui icon label">
+                <label>
+                    <h3>Filter by type: </h3>
+                    <select onChange={handleFilter}>
+                        <option value="placeholder">all</option>  
+                        <option value="normal">normal</option>
+                        <option value="fire">fire</option>
+                        <option value="water">water</option>
+                        <option value="grass">grass</option>
+                        <option value="electric">electric</option>
+                        <option value="ice">ice</option>
+                        <option value="fighting">fighting</option>
+                        <option value="poison">poison</option>
+                        <option value="ground">ground</option>
+                        <option value="flying">flying</option>
+                        <option value="psychic">psychic</option>
+                        <option value="bug">bug</option>
+                        <option value="rock">rock</option>
+                        <option value="ghost">ghost</option>
+                        <option value="dragon">dragon</option>
+                        <option value="fairy">fairy</option>
+                    </select>
+                </label>
+            </div>
+        </div>
+    )
+}
+
+
+export default Filter
